033ebfabba907757d5b6694611c5e74861328194
Tathagat Priyadarshi <100780858+priyadarshii786@users.noreply.github.com>
2023-11-17 23:17:05 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Roof Top/roof-top.cpp
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution
+{
+    public:
+    //Function to find maximum number of consecutive steps 
+    //to gain an increase in altitude with each step.
+    int maxStep(int A[], int N)
+    {
+       //Your code here
+       //jai ganesh
+       int ans = 0, res = 0;
+       for(int i=0; i<N-1; i++)
+       {
+            if(A[i+1] > A[i])
+            ans++;
+            
+            else
+            ans = 0;    
+            
+            res = max(res, ans);
+       }
+       
+       return res;
+    }
+};
+
+//{ Driver Code Starts.
+
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n;
+	    cin>>n;
+	    
+	    int a[n];
+	    
+	    for(int i=0;i<n;i++)
+	    cin>>a[i];
+	    Solution ob;
+	    cout << ob.maxStep(a, n) << endl;
+	}
+	return 0;
+}
+// } Driver Code Ends
