7c6de04a274e10bc1949964419ac0fa1439e74fe
Tathagat Priyadarshi <100780858+priyadarshii786@users.noreply.github.com>
2023-11-27 23:01:16 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Missing element of AP/missing-element-of-ap.cpp
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution{   
+public:
+    int findMissing(int arr[], int n) {
+        // code here
+        // jai ganesh
+        
+        // Binary Search approach: O(log n) Time complexity and O(1) Auxilary Space.
+        int d=(arr[n-1]-arr[0])/n;
+        int low=0,high=n-1;
+        while(low<=high)
+        {
+            int mid=(low+high)/2;
+            int val=arr[0]+(d*mid);
+            
+            if(arr[mid]==val)
+            low=mid+1;
+            
+            else
+            high=mid-1;
+        }
+        return arr[high]+d;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.findMissing(arr, n);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
